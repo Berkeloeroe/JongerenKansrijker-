3e82f1fec91585c9cafbf36223b5caaf4d95fa67
Randy <54804029+Berkeloeroe@users.noreply.github.com>
2021-05-28 18:08:25 +0200

    Add files via upload

--- JongerenKansrijker/statements.sql
+++ JongerenKansrijker/statements.sql
@@ -3,38 +3,41 @@ CREATE DATABASE JongerenKansrijker;
 USE JongerenKansrijker;
 
 CREATE TABLE activiteit (
-    activiteitcode VARCHAR(3) NOT NULL,
-    activiteit VARCHAR(3) NOT NULL
+    activiteitcode INT(11) NOT NULL,
+    activiteit VARCHAR(3) NOT NULL,
+    PRIMARY KEY (activiteitcode) 
 );
 
 CREATE TABLE jongere (
-    jongerecode VARCHAR(5) NOT NULL,
+    jongerecode INT(11) NOT NULL,
     roepnaam VARCHAR(20) NOT NULL,
     tussenvoegsel VARCHAR(7) NOT NULL,
     achternaam VARCHAR(25) NOT NULL,
-    inschrijfdatum DATE NOT NULL
+    inschrijfdatum DATE NOT NULL,
+    PRIMARY KEY (jongerecode) 
 );
 
 CREATE TABLE instituut (
-    instituutscode VARCHAR(5) NOT NULL,
+    instituutscode INT(11) NOT NULL,
     instituut VARCHAR(40) NOT NULL,
-    instituuttelefoon VARCHAR(11) NOT NULL
+    instituuttelefoon VARCHAR(11) NOT NULL,
+    PRIMARY KEY (instituutscode) 
 );
 
 CREATE TABLE jongereactiviteit (
-    jongerecode VARCHAR(3) NOT NULL,
-    actiecode VARCHAR(3) NOT NULL,
+    jongerecode INT(11) NOT NULL,
+    actiecode INT(11) NOT NULL,
     startdatum DATE NOT NULL,
     afgerond TINYINT(1) NOT NULL,
-    FOREIGN KEY jongerecode REFERENCES jongere(jongerecode)
+    FOREIGN KEY (jongerecode) REFERENCES jongere(jongerecode)
 ); 
 
 CREATE TABLE jongereinstituut (
-    jongerecode VARCHAR(5) NOT NULL,
-    instituutscode VARCHAR(5) NOT NULL,
+    jongerecode INT(11) NOT NULL,
+    instituutscode INT(11) NOT NULL,
     startdatum DATE NOT NULL,
-    FOREIGN KEY jongerecode REFERENCES jongere(jongerecode),
-    FOREIGN KEY instituutscode REFERENCES instituut(instituutscode)
+    FOREIGN KEY (jongerecode) REFERENCES jongere(jongerecode),
+    FOREIGN KEY (instituutscode) REFERENCES instituut(instituutscode)
 );
 
 
